eb0804e14f59293d9be6552355973549f03d2997
abhishek98s <abhishekshakya98.com@gmail.com>
2022-12-05 08:34:08 +0545

    first-commit

--- src/Pages/Careers.js
+++ src/Pages/Careers.js
@@ -1,21 +1,39 @@
 import {useState} from 'react';
 import './Careers.css'
+import axios from 'axios'
 
 function Careers(){
-
+    const[firstName, setFirstName ] = useState('');
+    const[lastName, setLastName] = useState('');
     const[email, setEmail] = useState('');
+    const[position, setPosition] = useState('');
+    const[addinfo, setAddinfo ] = useState('');
+    const[cv, setCv] = useState();
+    const[filename, setFilename] = useState(null)
+
     const[error, setError] = useState('');
     const err = document.querySelector('h6');
-    let inputFile = document.getElementById('file-input');
-    let fileNameField = document.getElementById('file-name');
+    // let form_box = document.getElementById('form_box');
+    // let fileNameField = document.getElementById('file-name');
 
+   
     const inputChange=(e)=>{
-        let uploadedFileName = e.target.files[0].name;
-        fileNameField.innerHTML = uploadedFileName;
+        setFilename(e.target.files[0].name);
+        // fileNameField.innerHTML = uploadedFileName;
+        setCv(e.target.files);
+        console.log(cv);
     }    
 
     function isValidEmail(email){
       return /\S+@\S+\.\S+/.test(email);
+    }
+
+    const firstnameChange=(e)=>{
+        setFirstName(e.target.value);
+    }
+
+    const lastnameChange=(e)=>{
+        setLastName(e.target.value);
     }
 
     const handleChange=(e)=>{
@@ -31,10 +49,50 @@ function Careers(){
         setEmail(e.target.value);
     }
 
+    const positionChange=(e)=>{
+        setPosition(e.target.value);
+    }
+
+    const addinfoChange=(e)=>{
+        setAddinfo(e.target.value);
+    }
+    
+    const cvChange=(e)=>{
+        setCv(e.target.files);
+    }
+
+    const formSubmit=(e)=>{
+        e.preventDefault();
+
+        let formData = new FormData(e.target) ;
+        console.log(formData)
+        const obj = {
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            position: position,
+            addinfo : addinfo
+        }
+
+        const strObj = JSON.stringify(obj);
+        const bod = {
+            strObj,
+            cv: cv}
+        console.log(bod)
+        
+        axios.post('http://localhost:2701/careers/add', bod)
+          .then(function (response) {
+            console.log(response);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+    }
 
     return(
         <>
-            <div className='form-box'>
+            <form className='form-box' onClick={formSubmit} encType="multipart/form-data" method='post'
+            action='localhost:2701/careers/add'>
                 <h1>Drop your CV</h1>
                 <p>Do you want to work with us? Please fill in your details below, we will get back to you</p>
                 <br/>
@@ -43,12 +101,12 @@ function Careers(){
 
                 <div className='form-items'>
                     <p>First Name</p>
-                    <input />
+                    <input onChange={firstnameChange} name='firstname'/>
                 </div>
 
                 <div className='form-items'>
                     <p>Last Name</p>
-                    <input />
+                    <input onChange={lastnameChange}/>
                 </div>
 
                 <div className='form-items'>
@@ -61,19 +119,19 @@ function Careers(){
 
                 <div className='form-items'>
                     <p>Position Applying For</p>
-                    <input />
+                    <input onChange={positionChange}/>
                 </div>
 
                 <div className='form-items'>
                     <p>Additional Information</p>
-                    <textarea />
+                    <textarea onChange={addinfoChange}/>
                 </div>
 
                 <div className='form-items'>
                     <p>Upload your CV Here</p>
                     <div className='cv-input'>
-                        <input id='file-input' onChange={inputChange} type='file' />
-                        <label for="file-input">
+                        <input id='file-input' onChange={inputChange} type='file' name='cvfile'/>
+                        <label htmlFor="file-input" name='cvfile'>
                             <svg width="151" height="156" viewBox="0 0 151 156" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path d="M121.618 65.5508C117.344 43.1977 98.3002 26.4167 75.4221 26.4167C57.258 26.4167 41.4822 37.0425 33.6257 52.5925C14.7073 54.6658 0 71.1877 0 91.2083C0 112.654 16.9071 130.083 37.7111 130.083H119.418C136.765 130.083 150.844 115.57 150.844 97.6875C150.844 80.5825 137.96 66.7171 121.618 65.5508ZM87.9925 84.7292V110.646H62.8518V84.7292H43.9963L75.4221 52.3333L106.848 84.7292H87.9925Z" fill="#414040"/>
                             </svg>
@@ -81,12 +139,12 @@ function Careers(){
                             <p>Browse files</p>
                             <p>Drag and drop files here</p>
                         </label>
-                        <p id='file-name'></p>
+                        <p id='file-name'>{filename}</p>
                     </div>
                 </div>
 
-                <button>Submit</button>
-            </div>
+                <input type='submit' name='submit'  value='submit' />
+            </form>
         </>
     )
 }
